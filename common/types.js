f21a566de50f6a5a5314b310be458d42c74fd6c1
venson <venson.hunt@gmail.com>
2023-02-21 23:18:59 +0800

    [refactor] constant

---
+++ common/types.js
@@ -0,0 +1,17 @@
+export const MemberLevel ={
+    INTERN : 'INTERN',
+    FORMER_MEMBER : 'FORMER_MEMBER',
+    CURRENT_MEMBER : 'CURRENT_MEMBER',
+    TECH : 'TECH',
+    PI : 'PI',
+}
+
+export const sortType ={
+    VIEWS: 'VIEWS',
+    CREATE: 'CREATE',
+    UPDATE:'UPDATE'
+}
+export const Language = {
+    CHINESE: 'CHINESE',
+    ENGLISH:'ENGLISH'
+}
