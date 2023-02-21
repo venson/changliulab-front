dce2168295ccb3762774e5240c8f50c211b8a50b
venson <venson.hunt@gmail.com>
2023-02-21 23:17:40 +0800

    [utils] utils file

--- utils/request.js
+++ utils/request.js
@@ -1,11 +1,13 @@
 import axios from 'axios'
 import cookie from 'js-cookie'
 
-const service = axios.create({
+const service = axios.create(
+    // {
     // baseURL: "http://localhost:9001",
-    baseURL: "http://localhost:8222",
-    timeout: 200000
-})
+    // baseURL: process.env.API_URL || "http://localhost:8222",
+    // timeout: 200000
+// }
+)
 service.interceptors.request.use(
     config =>{
         var token = cookie.get('changliuLab_token')
