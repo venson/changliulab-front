a0fee6501f7bd63e453b7c6dc9e5ac7346929c58
venson <venson.hunt@gmail.com>
2023-02-22 09:01:49 +0800

    [fix] start up shell

---
+++ shell/startup.sh
@@ -0,0 +1,27 @@
+CMD="yarn start"
+
+checkPid(){
+    echo "Checking if the server is running"
+    ps=$(ps | grep "${CMD}")
+    if [-n "$ps"]; then
+      echo "Following PID are the server"
+      pid=$(echo "$ps" | awk '{print $1}')
+    else
+    echo "No service is running"
+    pid=0
+    fi
+
+}
+
+startService(){
+  checkPid
+  if[ "${pid}" != 0]; then
+  echo " Kill running service"
+  killCurrentRunning
+  startService
+  else
+    echo "=========="
+    echo "start service"
+    nohup "yarn start"  >> "/dev/null" 2>&1 &
+  fi
+}
