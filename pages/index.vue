0ffb3fa54d82a6213bf0ee3376490c64f5edc910
venson <venson.hunt@gmail.com>
2023-02-25 23:21:08 +0800

    [fix] fix home page error while no banner was enabled.

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="px-4 max-w-7xl mx-auto divide-y">
-    <div v-if="banners">
+    <div v-if="banners && banners.length>0">
       <ImgCarousel :value="banners" />
       <!-- <LabCarousel  :value="banners"/> -->
     </div>
@@ -13,7 +13,7 @@
         </div> -->
       </div>
     </div>
-    <div class="py-4" v-if="memberList">
+    <div class="py-4" v-if="memberList && memberList.length>0">
       <div class="py-4 text-center font-semibold text-3xl">实验室成员</div>
       <div class="flex flex-col sm:flex-row">
         <div
@@ -26,7 +26,7 @@
       </div>
     </div>
 
-    <div class="py-4" v-if="courseList">
+    <div class="py-4" v-if="courseList && courseList.length>0">
       <div class="m-8 text-center font-semibold text-3xl">优秀课程</div>
       <div class="flex flex-col sm:flex-row">
         <div
@@ -52,12 +52,13 @@ export default {
   async asyncData({ $indexApi, $bannerApi }) {
     const index = await $indexApi.getIndexData();
     const banners = await $bannerApi.getBannerList();
-    // console.log(res.member);
+    console.log(index);
+    console.log(banners);
     return {
-      courseList: index && index.data.course,
-      activityList: index && index.data.activity,
-      memberList: index && index.data.member,
-      banners: banners && banners.data,
+      courseList: index.data && index.data.course,
+      activityList: index.data && index.data.activity,
+      memberList: index.data && index.data.member,
+      banners: banners.data && banners.data,
     };
   },
   data() {
