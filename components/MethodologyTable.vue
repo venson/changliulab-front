9b7333ce7d4271487a8268203f0c10e45af8a359
venson <venson.hunt@gmail.com>
2023-03-03 14:37:03 +0800

    [feature] add MethodologyTable component.

---
+++ components/MethodologyTable.vue
@@ -0,0 +1,35 @@
+
+<template>
+    <div class="m-4 ">
+      <table class="table-auto border-collapse w-full">
+        <tbody>
+          <tr v-for="(methodology, index) in value" :key="index" class="mt-4">
+            <td class="border-b p-4">
+              <div >
+                <nuxt-link class="text-green-800 text-base hover:text-green-600 " :to="`/methodology/${methodology.id}`">
+                {{ methodology.title }}
+
+                </nuxt-link>
+                <!-- <a :href="`/activity/${activity.id}`">
+                {{ activity.title }}
+
+                </a> -->
+              </div>
+          </td>
+
+          </tr>
+        </tbody>
+      </table>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'MethodologyTable',
+        props: ['value'],
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
