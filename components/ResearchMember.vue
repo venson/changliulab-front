7a87623cb788f77c1d1c967d5eeff0bcd5f9bf11
venson <venson.hunt@gmail.com>
2023-03-03 14:37:33 +0800

    [freature] make single research to multi

---
+++ components/ResearchMember.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="flex flex-row" v-if="value && value.length >0">
+        <div class="flex items-center justify-center" v-for="(member, index) in value" :key="index">
+            <nuxt-link :to="{path:`/groupmember/${member.id}`}" >
+                <div class="flex p-4 items-center justify-center">
+                            <nuxt-img 
+                            class="rounded-full mr-2" 
+                            :class="index===0? 'border-green-500 w-10 h-10 border-4 border-solid':'w-7 h-7'" 
+                            :src="member.avatar"></nuxt-img>
+                            <span>{{member.name}}</span>
+                </div>
+            </nuxt-link>
+        </div>
+
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'ResearchMember',
+        props:['value']
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ components/ResearchViewer.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="divide-y">
+    <div class="py-8" v-for="(research, index) in value" :key="index">
+        <div class="flex justify-between items-center px-16">
+            <div class="text-4xl">{{ research.title}}</div>
+            <ResearchMember :value="research.members"/>
+        </div>
+      <div class="p-8" v-html="research.html"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+import ResearchMember from './ResearchMember.vue';
+
+export default {
+    name: "ResearchViewer",
+    props: ["value"],
+    components: { ResearchMember }
+}
+</script>
+
+<style lang="scss" scoped></style>
