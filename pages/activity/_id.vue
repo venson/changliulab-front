3d7640c5855ecd3b281e828014113eeeba4daf7c
venson <venson.hunt@gmail.com>
2023-03-13 01:22:06 +0800

    [fix] add activity title and date

--- pages/activity/_id.vue
+++ pages/activity/_id.vue
@@ -2,6 +2,10 @@
 
   <div class="flex justify-center items-center">
     <div class="w-full max-w-7xl ">
+        <div class="p-4">
+        <div class="text-center text-3xl p-2">{{ title }}</div>
+        <div class="text-center text-base">{{ activityDate }}</div>
+        </div>
       <div class=" p-8 markdown-body" v-html="activity"></div>
     </div>
   </div>
@@ -12,16 +16,18 @@ import { base64ToBytes } from '@/utils/base64';
 export default{
   asyncData({ $activityApi,params}) {
     return $activityApi.getActivity(params.id).then((response) => {
-      console.log(response)
+    //   console.log(response)
       let html
       try {
-        const bytes = base64ToBytes(response.data.publishedHtmlBrBase64);
+        const bytes = base64ToBytes(response.data.htmlBrBase64);
         html = pako.inflate(bytes, { to: "string" });
       } catch (error) {
         html = " ";
       }
       return {
         activity:html,
+        title:response.data.title,
+        activityDate:response.data.activityDate,
       };
     });
   },
